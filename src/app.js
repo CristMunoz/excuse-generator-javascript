71eb30d7d4dc6fd73e06f280601207a1b9290f96
Cristian Muñoz <104040085+CristMunoz@users.noreply.github.com>
2024-03-13 01:07:10 +0000

    Modificando los archivos index.html y app.js

--- src/app.js
+++ src/app.js
@@ -2,10 +2,39 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// Arrays utilizados para el Excuse Generator
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// Función para generar excusas
+function excuseGenerator(who, action, what, when) {
+  let randomWho = Math.floor(Math.random() * who.length);
+  let randomAction = Math.floor(Math.random() * action.length);
+  let randomWhat = Math.floor(Math.random() * what.length);
+  let randomWhen = Math.floor(Math.random() * when.length);
+  return (
+    who[randomWho] +
+    " " +
+    action[randomAction] +
+    " " +
+    what[randomWhat] +
+    " " +
+    when[randomWhen]
+  );
+}
+
+// Función para cargar el generador
+function loadGenerator() {
+  let excuse = document.querySelector("#excuse");
+  excuse.innerHTML = excuseGenerator(who, action, what, when);
+}
+
+window.onload = loadGenerator;
